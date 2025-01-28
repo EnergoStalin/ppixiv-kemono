c77644fb53aeb673194304be253ab0e21780c29d
EnergoStalin <ens.stalin@gmail.com>
2025-01-28 13:02:32 +0300

    fix(fanbox): post url handling

--- src/links/fanbox.ts
+++ src/links/fanbox.ts
@@ -2,13 +2,17 @@ import { memoizedRegexRequest } from "./memo"
 import { makeUrl } from "./url"
 
 export function fanbox(link: UserLink, extraLinks: UserLink[], userId: number) {
+	const url = new URL(link.url)
+	url.pathname = ""
+
 	memoizedRegexRequest(
 		(id) => {
-			extraLinks.push(makeUrl("kemono", "fanbox", id))
-			extraLinks.push(makeUrl("nekohouse", "fanbox", id))
+			const cid = id === "undefined" ? userId : id
+			extraLinks.push(makeUrl("kemono", "fanbox", cid))
+			extraLinks.push(makeUrl("nekohouse", "fanbox", cid))
 		},
 		userId,
-		link.url.toString(),
+		url.toString(),
 		/fanbox\/public\/images\/creator\/([0-9]+)\/cover/,
 	)
 }
