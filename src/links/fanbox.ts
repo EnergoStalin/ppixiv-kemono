7c378081ec0c262bdee7bca5768782018ac64415
EnergoStalin <ens.stalin@gmail.com>
2025-08-26 11:41:58 +0300

    fix: pixiv -> fanbox link
    https://www.pixiv.net/fanbox/creator/<id>

--- src/links/fanbox.ts
+++ src/links/fanbox.ts
@@ -12,13 +12,21 @@ const fanboxId = memoize(async (creatorId: string) => {
 		.catch(console.error)
 })
 
+function makeFanboxUrls(extraLinks: UserLink[], id: number | string) {
+	makeUrls(extraLinks, "fanbox", id)
+}
+
 export function fanbox(link: UserLink, extraLinks: UserLink[], userId: number) {
 	const url = new URL(link.url)
-	let creatorId = url.host.split(".").shift()
-	if (creatorId && (creatorId === "fanbox" || creatorId === "www")) {
-		creatorId = url.pathname.replace(/^[/@]*/, "")
-		if (creatorId.length === 0) return
-	}
 
-	fanboxId((id) => makeUrls(extraLinks, "fanbox", id), userId, creatorId)
+	if (url.host.includes("pixiv.net")) {
+		makeFanboxUrls(extraLinks, url.pathname.split('/').pop() ?? "0")
+	} else {
+		let creatorId = url.host.split(".").shift()
+		if (creatorId && (creatorId === "fanbox" || creatorId === "www")) {
+			creatorId = url.pathname.replace(/^[/@]*/, "")
+			if (creatorId.length === 0) return
+		}
+		fanboxId((id) => makeUrls(extraLinks, "fanbox", id), userId, creatorId)
+	}
 }
